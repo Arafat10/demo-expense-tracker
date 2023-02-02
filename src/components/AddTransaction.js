f21b29e52a2a16f2de5e0f2addc9cc3700ffbe72
Arafat10 <arafat.nabil.10@gmail.com>
2023-02-02 20:56:18 +0600

    final

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,8 +1,24 @@
 import React, {useState} from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
 export const AddTransaction = () => {
   const [text, setText] = useState('');
   const [amount, setAmount] = useState(0);
+
+  /*global useContext*/
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount
+    }
+
+    addTransaction(newTransaction);
+  }
 
   return (
     <>
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -2,12 +2,7 @@ import React,{createContext, userReducer} from "react";
 import AppReducer from './AppReducer';
 
 const initialState = {
-    transactions: [
-          { id: 1, text: 'Flower', amount: -20 },
-          { id: 2, text: 'Salary', amount: 300 },
-          { id: 3, text: 'Book', amount: -10 },
-          { id: 4, text: 'Camera', amount: 150 }
-        ]  
+    transactions: []  
 }
 
 export const GlobalContext = createContext(initialState);
@@ -15,6 +10,20 @@ export const GlobalContext = createContext(initialState);
 /*global useReducer */
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  function deleteTransaction(id) {
+    dispatch({
+      type: 'DELETE_TRANSACTION',
+      payload: id
+    });
+  }
+
+  function addTransaction(transaction) {
+    dispatch({
+      type: 'ADD_TRANSACTION',
+      payload: transaction
+    });
+  }
 
   return (<GlobalContext.Provider value={{
     transactions:state.transactions
